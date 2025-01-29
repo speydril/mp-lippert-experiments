9462691299084ab70d853f572c6ccbc5aa7d644b
LeonHermann322 <leon.hermann@me.com>
2025-01-29 10:12:43 +0100

    online st update

--- online_st_run.py
+++ online_st_run.py
@@ -65,7 +65,6 @@ def run_teacher_prompt_encoder_pretraining(n_teacher_samples: str):
 
     print(f"Training teacher prompt encoder...")
     tags = '["PromptEncoder", "Finetuning", "TeacherTraining", "VesselSeg"]'
-    # TODO: --wandb_tags={shlex.quote(tags)} --use_wandb=true --wandb_experiment_name=gt_vessels_onlineST_teacher_{n_teacher_samples}_samples
     train_teacher_cmd = f"python run.py --experiment_id=multi_ds_vessel --sam_model=vit_b --learning_rate=0.0003 --prompt_encoder_lr=0.003 --image_encoder_lr=0.0 --mask_decoder_lr=0.0 --batch_size=3 --epochs=1 --weight_decay=1e-4 --early_stopping_patience=10 --visualize_n_segmentations=5 --gamma=0.9 --step_size=1 --best_model_metric=IoU --minimize_best_model_metric=false  --amp=true --results_subdir_name={teacher_experiment_subdir_name}"
 
     if n_teacher_samples != "all":
@@ -91,13 +90,7 @@ def run_teacher_pretraining(n_teacher_samples: str):
 
     print(f"Training teacher...")
     tags = '["FullModel", "Finetuning", "TeacherTraining", "VesselSeg", "GT"]'
-    # TODO: --wandb_tags={shlex.quote(tags)} --use_wandb=true --wandb_experiment_name=gt_vessels_onlineST_teacher_{n_teacher_samples}_samples
-    train_teacher_cmd = f"""python run.py --experiment_id=multi_ds_vessel --sam_model=vit_b 
-    --learning_rate=0.0003 --prompt_encoder_lr=0.0001 --image_encoder_lr=0.00001
-    --mask_decoder_lr=0.0001 --batch_size=3 --epochs=1 --weight_decay=1e-4 --early_stopping_patience=10
-    --visualize_n_segmentations=5 --gamma=0.95 --step_size=3 --best_model_metric=IoU
-    --minimize_best_model_metric=false --prompt_encoder_checkpoint={teacher_checkpoint_dir}/prompt_encoder.pt
-    --amp=true --results_subdir_name={teacher_experiment_subdir_name}"""
+    train_teacher_cmd = f"python run.py --experiment_id=multi_ds_vessel --sam_model=vit_b --learning_rate=0.0003 --prompt_encoder_lr=0.0001 --image_encoder_lr=0.00001 --mask_decoder_lr=0.0001 --batch_size=3 --epochs=1 --weight_decay=1e-4 --early_stopping_patience=10 --visualize_n_segmentations=5 --gamma=0.95 --step_size=3 --best_model_metric=IoU --minimize_best_model_metric=false --prompt_encoder_checkpoint={teacher_checkpoint_dir}/prompt_encoder.pt --amp=true --results_subdir_name={teacher_experiment_subdir_name}"
 
     if n_teacher_samples != "all":
         train_teacher_cmd += f" --limit_train_samples={n_teacher_samples}"
@@ -115,9 +108,9 @@ def run_student_st(teacher_checkpoint: str, epochs: int, learning_rate: float):
         f"onlineST_student_{n_teacher_samples}_samples_{dir_suffix}"
     )
     print("Teacher checkpoint:", teacher_checkpoint)
-    # tags = '["FullModel", "OnlineST", "VesselSeg", "PseudoLabels"]'
+    tags = '["FullModel", "OnlineST", "VesselSeg", "PseudoLabels"]'
     exec(
-        f"python run.py --experiment_id=self_learning_experiment --limit=100 --sam_model=vit_b --learning_rate={str(learning_rate)} --batch_size=20 --epochs={str(epochs)} --weight_decay=1e-4 --gamma=0.9 --step_size=2 --best_model_metric=IoU --minimize_best_model_metric=false --drive_test_equals_val=false --amp=true --augment_train=true --ema_decay=0.95 --from_checkpoint={teacher_checkpoint} --results_subdir_name={student_experiment_subdir_name} --return_best_model=false"
+        f"python run.py --experiment_id=self_learning_experiment --use_wandb=false --wandb_tags={shlex.quote(tags)} --wandb_experiment_name=vessels_gt_oneline_st_{n_teacher_samples}_samples --limit=5000 --sam_model=vit_b --learning_rate={str(learning_rate)} --batch_size=20 --epochs={str(epochs)} --weight_decay=1e-4 --gamma=0.9 --step_size=2 --best_model_metric=IoU --minimize_best_model_metric=false --drive_test_equals_val=false --amp=true --augment_train=true --ema_decay=0.95 --from_checkpoint={teacher_checkpoint} --results_subdir_name={student_experiment_subdir_name} --return_best_model=false"
     )
     student_checkpoint_dir = (
         "/dhc/home/leon.hermann/MP/mp-lippert-experiments"
@@ -140,7 +133,7 @@ def run_full_fine_tuning(
 ):
     subdir_name = f"{prefix}_fft_{n_teacher_samples}_samples_{dir_suffix}"
     tags = '["FullModel", "FinalFinetuning", "GT", "VesselSeg"]'
-    cmd = f"python run.py --experiment_id=multi_ds_vessel --wandb_tags={shlex.quote(tags)} --wandb_experiment_name=vessels_gt_{prefix}_fft_{n_teacher_samples}_samples --use_wandb=true --sam_model=vit_b --learning_rate={str(learning_rate)} --batch_size=4 --epochs=1 --weight_decay=1e-4 --gamma=0.9 --step_size=2 --best_model_metric=IoU --minimize_best_model_metric=false --from_checkpoint={checkpoint_path} --drive_test_equals_val=false --amp=true --results_subdir_name={subdir_name}"
+    cmd = f"python run.py --experiment_id=multi_ds_vessel --wandb_tags={shlex.quote(tags)} --wandb_experiment_name=vessels_gt_{prefix}_fft_{n_teacher_samples}_samples --use_wandb=true --sam_model=vit_b --learning_rate={str(learning_rate)} --batch_size=4 --epochs=20 --weight_decay=1e-4 --gamma=0.9 --step_size=2 --best_model_metric=IoU --minimize_best_model_metric=false --from_checkpoint={checkpoint_path} --drive_test_equals_val=false --amp=true --results_subdir_name={subdir_name}"
     if n_teacher_samples != "all":
         cmd += f" --limit_train_samples={n_teacher_samples}"
     exec(cmd)
